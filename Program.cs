91294be71a979e4245b79211564f2c6fc767eb2b
teddyzxcv <pan2691712848@gmail.com>
2020-11-07 11:41:53 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Earth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
